df12466a53fd645bcd3571e1419c13e66133ca05
Simon Emms <simon@simonemms.com>
2024-11-22 22:29:21 +0000

    feat: style the homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<nav class="navbar" aria-label="main navigation">
+	<div class="navbar-end">
+		<div class="navbar-item">
+			<a
+				href="https://github.com/mrsimonemms/chartversion.app"
+				class="has-text-dark"
+				target="_blank"
+				aria-label="Open source code"
+			>
+				<span class="icon is-medium">
+					<i class="mdi mdi-36px mdi-github"></i>
+				</span>
+			</a>
+		</div>
+	</div>
+</nav>
+
+<div class="container is-fluid">
+	<div class="columns is-mobile">
+		<div class="column is-6-desktop is-offset-3-desktop">
+			<div class="has-text-centered">
+				<h1 class="title">Chart Version</h1>
+
+				<div class="mx-4">
+					<p class="subtitle">
+						Finding all the available apps and versions of a Helm chart is a
+						pain in the arse. So I built a tool that would list them for you.
+					</p>
+
+					<p class="subtitle">
+						Enter the chart's URL (<code>https://</code> or <code>ocr://</code>)
+						and it'll list what's in there.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="is-flex-grow-1">
+		<slot />
+	</div>
+</div>
+
+<div class="footer has-text-centered">
+	<p>
+		&copy; 2024 Made with
+		<span class="icon has-text-danger"><i class="mdi mdi-heart"></i></span>
+		by
+		<a href="https://simonemms.com" target="_blank">Simon Emms</a>
+	</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-	documentation
-</p>
+<script lang="ts">
+	const defaultChart = 'https://charts.jetstack.io';
+</script>
+
+<div class="columns is-mobile">
+	<div class="column is-4-desktop is-offset-4-desktop">
+		<label class="label" for="search">Enter the Helm repository URL</label>
+		<div class="field has-addons has-addons-centered">
+			<div class="control is-expanded">
+				<input
+					id="search"
+					class="input"
+					type="text"
+					placeholder={defaultChart}
+				/>
+				<p class="help has-text-danger">Some error message</p>
+			</div>
+			<div class="control">
+				<button class="button is-primary">Search</button>
+			</div>
+		</div>
+	</div>
+</div>
